cf895bb1d95ff73b647b9b07ae2e3fcc067cea42
Madison J <madijrdn@gmail.com>
2022-05-13 11:18:49 -0700

    add project 2

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include <fstream>
 #include "lexer.h"
+#include "parser.h"
 
 using namespace std;
 
@@ -41,6 +42,7 @@ int main()
     int prevState = REJECT;
     string currentToken = "";
     bool blockComment = false;
+    bool hasSemicolon = false;
 
     // use a loop to scan each line in the file
     while (getline(infile, expression))
@@ -49,12 +51,45 @@ int main()
         // string, and unknown tokens
         tokens = lexer(expression, prevState, currentState, currentToken);
 
+        string currentLine;
+        char *cstr;
+        hasSemicolon = false;
+
+
         // display the tokens to the screen
         for (unsigned x = 0; x < tokens.size(); ++x)
         {
             outfile << std::left << std::setw(10) << tokens[x].lexemeName << "\t\t"
                  << tokens[x].token << endl;
+            if (tokens[x].lexemeName == "IDENTIFIER"){
+                tokens[x].token = 'i';
+            }
+
+            //check if has semicolon and don't include in parsing
+            if (tokens[x].token == ";"){
+                tokens[x].token = '$'; 
+                hasSemicolon = true;
+            }
+            cstr = &tokens[x].token[0];
+            currentLine.push_back(cstr[0]);
+
         }
+        
+        // don't parse if missing semicolon
+        if (!hasSemicolon){
+            outfile << "\nERROR:\n\t No Semicolon!\n";
+            outfile << "\n##########################################################################\n";
+        }else{
+            std::streambuf *oldbuf = std::cout.rdbuf();
+            std::cout.rdbuf(outfile.rdbuf()); 
+
+            parser(currentLine);
+
+            //reset back to standard input
+            std::cout.rdbuf(oldbuf);
+        }
+        
+
     }
 
     infile.close();
